fe38c4280e97d2d34b961333d586bf0374df18f8
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-01 10:09:34 +0200

    Programmierung der Aufgaben Spannweite & Primfaktorzerlegung

--- 01_ErsteAlgorithmen/src/ErsteAlgorithmen.java
+++ 01_ErsteAlgorithmen/src/ErsteAlgorithmen.java
@@ -1,14 +1,55 @@
+import java.util.ArrayList;
+
+import org.junit.jupiter.params.aggregator.ArgumentAccessException;
+
 public class ErsteAlgorithmen {
 
     public static int spannweite(int[] folge) {
         //Bitte hier die Loesung ergaenzen
-        return -1;
+        //Die O-Notation dieses Algorithmus ist O(n)
+        if (folge.length > 1){
+            int kleinsteZahl = folge[0];
+            int groessteZahl = folge[0];
+
+            for(int i = 1; i <= folge.length-1; i++ ){
+                if(kleinsteZahl > folge[i]){
+                    kleinsteZahl = folge[i];
+                }
+                if(groessteZahl < folge[i]){
+                    groessteZahl = folge[i];
+                }
+            }
+            return groessteZahl - kleinsteZahl;
+        }
+        return 0;
     }
     
     
     public static int[] primfaktorzerlegung(int zahl) {
         //Bitte hier die Loesung ergaenzen
-        return null;
+        ArrayList<Integer> zahlen = new ArrayList<Integer>();
+        int primfaktor = 2;
+
+        while(zahl > 1){
+            if(zahl % primfaktor == 0){
+                zahlen.add(primfaktor);
+                zahl = zahl / primfaktor;
+            } else {
+                if(primfaktor == 2){
+                    primfaktor = 3;
+                } else {
+                    primfaktor = primfaktor + 2;
+                }
+            }
+        }
+
+        int primfaktoren[] = new int[zahlen.size()];
+
+        for(int i = 0; i < zahlen.size(); i++){
+            primfaktoren[i] = zahlen.get(i);
+        }
+
+        return primfaktoren;
     }
     
     public static double quadratwurzel(double x, double eps) {
